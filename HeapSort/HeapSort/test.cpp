b6a622345db1eeeff2c461b557feeabec61d58f4
YIUECHEN <2268691528@qq.com>
2020-02-25 21:58:00 +0800

    change

--- HeapSort/HeapSort/test.cpp
+++ HeapSort/HeapSort/test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//堆化：使一个元素符合堆的要求
 void heapify(int a[], int size, int index){
 	int left = index * 2 + 1;
 	int right = index * 2 + 2;
@@ -8,37 +9,45 @@ void heapify(int a[], int size, int index){
 	if (left > size||right>size){
 		return;
 	}
-	max = a[left] > a[right] ? left : right;
-	if (a[index] > a[max]){
+	max = a[left] >= a[right] ? left : right;
+	if (a[index] >= a[max]){
 		return;
 	}
 	else{
 		swap(a[index], a[max]);
-	}
-	heapify(a, size, max);
-}
+		heapify(a, size, max);
 
+	}
+
+}
+//建堆
 void creatHeap(int a[], int size){
-	for (int i = (size - 2) / 2; i >= 0; i--){
+	for (int i = (size / 2)-1; i >= 0; i--){
 		heapify(a, size, i);
 	}
 	return;
 }
 
+//堆排序
 void HeapSort(int a[], int size){
 	creatHeap(a, size);
-	for (int i = size; i >= 0; i--){
+	for (int i = size-1; i >= 0; i--){
 		swap(a[0], a[i]);
 		heapify(a, i - 1, 0);
 	}
 }
 int main(){
-	int a[] = { 8, 3, 12, 7, 5, 25, 94, 33 ,44};
+	int a[] = { 8, 3, 12, 7, 5, 25, 94, 33 };
 	int len = sizeof(a) / sizeof(int);
-	HeapSort(a, len);
-	for (auto e : a){
+	for (auto &e : a){
 		cout << e << " ";
 	}
+	cout << endl;
+	HeapSort(a, len);
+	for (auto &e : a){
+		cout << e << " ";
+	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
